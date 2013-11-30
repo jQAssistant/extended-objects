1ce5bcfe2dffdc3c5c99c8ba33a781ab98856d3b
Dirk Mahler <dirk.mahler@buschmais.com>
2013-11-30 23:54:34 +0100

    #7 added provider for Neo4j embedded

--- api/src/main/java/com/buschmais/cdo/api/bootstrap/CdoUnitFactory.java
+++ api/src/main/java/com/buschmais/cdo/api/bootstrap/CdoUnitFactory.java
@@ -75,20 +75,25 @@ public class CdoUnitFactory {
                 types.add(ClassHelper.getType(typeName));
             }
             ValidationModeType validationModeType = cdoUnitType.getValidationMode();
-            CdoUnit.ValidationMode validationMode;
-            switch (validationModeType) {
-                case NONE:
-                    validationMode = CdoUnit.ValidationMode.NONE;
-                    break;
-                case AUTO:
-                    validationMode = CdoUnit.ValidationMode.AUTO;
-                    break;
-                default:
-                    throw new CdoException("Unknown validation mode type " + validationModeType);
+            CdoUnit.ValidationMode validationMode = CdoUnit.ValidationMode.AUTO;
+            if (validationModeType != null) {
+                switch (validationModeType) {
+                    case NONE:
+                        validationMode = CdoUnit.ValidationMode.NONE;
+                        break;
+                    case AUTO:
+                        validationMode = CdoUnit.ValidationMode.AUTO;
+                        break;
+                    default:
+                        throw new CdoException("Unknown validation mode type " + validationModeType);
+                }
             }
             Properties properties = new Properties();
-            for (PropertyType propertyType : cdoUnitType.getProperties().getProperty()) {
-                properties.setProperty(propertyType.getName(), propertyType.getValue());
+            PropertiesType cdoUnitTypeProperties = cdoUnitType.getProperties();
+            if (cdoUnitTypeProperties != null) {
+                for (PropertyType propertyType : cdoUnitTypeProperties.getProperty()) {
+                    properties.setProperty(propertyType.getName(), propertyType.getValue());
+                }
             }
             CdoUnit cdoUnit = new CdoUnit(name, description, url, provider, types, validationMode, properties);
             cdoUnits.put(name, cdoUnit);
