41d46620320858b15166a71df0ea6f0e934f371b
Tobias Israel <tobias.israel@buschmais.com>
2013-12-06 16:52:29 +0100

    * add validation for cdo.xml * minor xsd cosmetics

---
+++ api/src/main/java/com/buschmais/cdo/api/bootstrap/CdoUnitValidationHandler.java
@@ -0,0 +1,41 @@
+package com.buschmais.cdo.api.bootstrap;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.bind.ValidationEvent;
+import javax.xml.bind.ValidationEventHandler;
+
+public class CdoUnitValidationHandler implements ValidationEventHandler {
+
+    private List<String> errorMessages = new ArrayList<>();
+
+    @Override
+    public boolean handleEvent(ValidationEvent event) {
+        if (event.getSeverity() == ValidationEvent.ERROR || event.getSeverity() == ValidationEvent.FATAL_ERROR) {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append("line ");
+            stringBuilder.append(event.getLocator().getLineNumber());
+            stringBuilder.append(": ");
+            stringBuilder.append(event.getMessage());
+            this.errorMessages.add(stringBuilder.toString());
+            return true;
+        } else {
+            //ignore validation warnings
+            return true;
+        }
+    }
+
+    public boolean passesValidation() {
+        return this.errorMessages.size() == 0;
+    }
+
+    public String getValidationMessages() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (String errorMessage : errorMessages) {
+            stringBuilder.append(errorMessage);
+            stringBuilder.append(System.lineSeparator());
+        }
+        return stringBuilder.toString();
+    }
+}
