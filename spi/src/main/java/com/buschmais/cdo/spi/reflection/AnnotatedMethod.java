0927f7921e58f35bed695e6f1cb0a72e321754bf
Dirk Mahler <dirk.mahler@buschmais.com>
2013-12-19 12:34:41 +0100

    #34 SPI clean up and documentation

--- spi/src/main/java/com/buschmais/cdo/spi/reflection/AnnotatedMethod.java
+++ spi/src/main/java/com/buschmais/cdo/spi/reflection/AnnotatedMethod.java
@@ -2,6 +2,8 @@ package com.buschmais.cdo.spi.reflection;
 
 import java.lang.reflect.Method;
 
+/**
+ * Represents an annotated method.
+ */
 public interface AnnotatedMethod extends AnnotatedElement<Method> {
-
 }
