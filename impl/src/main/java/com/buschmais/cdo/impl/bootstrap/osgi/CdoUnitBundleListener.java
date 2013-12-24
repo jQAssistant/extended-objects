3fbe933591aef2d21117629d4011273a62a96a34
Lars Martin <lars.martin@smb-tec.com>
2013-12-24 18:15:24 +0100

    - implement BundleListener to register OSGi service per found CdoUnit

--- impl/src/main/java/com/buschmais/cdo/impl/bootstrap/osgi/CdoUnitBundleListener.java
+++ impl/src/main/java/com/buschmais/cdo/impl/bootstrap/osgi/CdoUnitBundleListener.java
@@ -1,7 +1,11 @@
 package com.buschmais.cdo.impl.bootstrap.osgi;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.Dictionary;
 import java.util.Enumeration;
+import java.util.Hashtable;
+import java.util.List;
 
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleActivator;
@@ -11,46 +15,78 @@ import org.osgi.framework.BundleListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.buschmais.cdo.api.CdoManagerFactory;
+import com.buschmais.cdo.api.bootstrap.CdoUnit;
+import com.buschmais.cdo.impl.CdoManagerFactoryImpl;
+import com.buschmais.cdo.impl.bootstrap.CdoUnitFactory;
+
 public class CdoUnitBundleListener implements BundleActivator, BundleListener {
 
-	private static final Logger LOGGER = LoggerFactory
-			.getLogger(CdoUnitBundleListener.class);
+    private static final Logger LOGGER = LoggerFactory
+            .getLogger(CdoUnitBundleListener.class);
 
-	public void start(BundleContext context) throws Exception {
-		context.addBundleListener(this);
-	}
+    @Override
+    public void start(BundleContext context) throws Exception {
+        context.addBundleListener(this);
+    }
 
-	public void stop(BundleContext context) throws Exception {
-		context.removeBundleListener(this);
-	}
+    @Override
+    public void stop(BundleContext context) throws Exception {
+        context.removeBundleListener(this);
+    }
 
-	public void bundleChanged(BundleEvent event) {
-		switch (event.getType()) {
-		case BundleEvent.STARTED:
-			deployCdoUnit(event.getBundle());
-			break;
-		case BundleEvent.STOPPED:
-			undeployCdoUnit(event.getBundle());
-			break;
-		}
-	}
+    @Override
+    public void bundleChanged(BundleEvent event) {
+        switch (event.getType()) {
+        case BundleEvent.STARTED:
+            deployCdoUnit(event.getBundle());
+            break;
+        case BundleEvent.STOPPED:
+            undeployCdoUnit(event.getBundle());
+            break;
+        }
+    }
 
-	private void deployCdoUnit(Bundle bundle) {
-		Enumeration<?> e = bundle.findEntries("META-INF", "cdo.xml", false);
-		if (e.hasMoreElements()) {
-			if (LOGGER.isInfoEnabled()) {
-				LOGGER.info(
-						"[CdoUnitDeployer] Deploying CdoUnit in bundle '{0}'",
-						bundle.getSymbolicName());
-			}
-			URL cdoUnitUrl = (URL) e.nextElement();
-			String bundleLocation = bundle.getLocation();
-			LOGGER.info("cdoUnitUrl: {0}", cdoUnitUrl);
-			LOGGER.info("bundleLocation: {0}", bundleLocation);
-		}
-	}
+    private void deployCdoUnit(Bundle bundle) {
+        Enumeration<?> e = bundle.findEntries("META-INF", "cdo.xml", false);
+        if (e != null && e.hasMoreElements()) {
+            if (LOGGER.isInfoEnabled()) {
+                LOGGER.info(
+                        "[CdoUnitBundleListener] Deploying CdoUnits in bundle '{0}'",
+                        bundle.getSymbolicName());
+            }
+            URL cdoUnitUrl = (URL) e.nextElement();
+            try {
+                List<CdoUnit> cdoUnits = CdoUnitFactory.getInstance()
+                        .getCdoUnits(cdoUnitUrl);
+                for (CdoUnit cdoUnit : cdoUnits) {
+                    if (LOGGER.isDebugEnabled()) {
+                        LOGGER.debug(
+                                "[CdoUnitBundleListener] Found CdoUnit '{0}'",
+                                cdoUnit.getName());
+                    }
+                    CdoManagerFactory cdoManagerFactory = new CdoManagerFactoryImpl(
+                            cdoUnit);
+                    Dictionary<String, Object> p = new Hashtable<String, Object>();
+                    p.put("name", cdoUnit.getName());
+                    bundle.getBundleContext().registerService(
+                            CdoManagerFactory.class, cdoManagerFactory, p);
 
-	private void undeployCdoUnit(Bundle bundle) {
-	}
+                    if (LOGGER.isDebugEnabled()) {
+                        LOGGER.debug(
+                                "[CdoUnitBundleListener] Registered service for CdoUnit '{0}'",
+                                cdoUnit.getName());
+                    }
+                }
+            } catch (IOException ioe) {
+                if (LOGGER.isErrorEnabled()) {
+                    LOGGER.error("Error while loading CdoUnit", ioe);
+                }
+            }
+        }
+    }
+
+    private void undeployCdoUnit(Bundle bundle) {
+    }
 
 }
