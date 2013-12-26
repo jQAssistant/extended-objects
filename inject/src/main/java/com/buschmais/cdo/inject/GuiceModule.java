85c5c0f83992f7fab9dbfadb1d7b33078d3c2903
Thomas Baum <thomas.baum@atns.de>
2013-12-26 18:09:51 +0100

    cleanup guice

--- inject/src/main/java/com/buschmais/cdo/inject/GuiceModule.java
+++ inject/src/main/java/com/buschmais/cdo/inject/GuiceModule.java
@@ -1,13 +1,27 @@
 package com.buschmais.cdo.inject;
 
+import com.buschmais.cdo.api.CdoManagerFactory;
+import com.buschmais.cdo.api.bootstrap.Cdo;
 import com.google.inject.AbstractModule;
-import com.google.inject.matcher.Matchers;
+import com.google.inject.Provides;
 
 public class GuiceModule extends AbstractModule {
 
-	@Override
-	protected void configure() {
-		bindListener(Matchers.any(), new CdoManagerTypeListener());
-	}
+    private final String defaultUnit;
 
+    public GuiceModule() {
+        this("default");
+    }
+
+    public GuiceModule(String defaultUnit) {
+        this.defaultUnit = defaultUnit;
+    }
+
+    @Override
+    protected void configure() {
+    }
+
+    @Provides CdoManagerFactory cdoManagerFactory() {
+        return Cdo.createCdoManagerFactory(defaultUnit);
+    }
 }
