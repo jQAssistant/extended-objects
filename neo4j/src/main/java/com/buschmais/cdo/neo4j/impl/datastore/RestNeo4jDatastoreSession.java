e0ae79c0290009af168800edd49a0b7cb3187337
Dirk Mahler <dirk.mahler@buschmais.com>
2014-01-14 13:26:59 +0100

    #10 minor cleanup

--- neo4j/src/main/java/com/buschmais/cdo/neo4j/impl/datastore/RestNeo4jDatastoreSession.java
+++ neo4j/src/main/java/com/buschmais/cdo/neo4j/impl/datastore/RestNeo4jDatastoreSession.java
@@ -50,7 +50,7 @@ public class RestNeo4jDatastoreSession extends AbstractNeo4jDatastoreSession<Res
         RestCypherQueryEngine restCypherQueryEngine = new RestCypherQueryEngine(restAPI);
         QueryResult<Map<String, Object>> queryResult = restCypherQueryEngine.query(getCypher(expression), parameters);
         final Iterator<Map<String, Object>> iterator = queryResult.iterator();
-        return new ResultIterator() {
+        return new ResultIterator<Map<String, Object>>() {
 
             @Override
             public boolean hasNext() {
@@ -58,7 +58,7 @@ public class RestNeo4jDatastoreSession extends AbstractNeo4jDatastoreSession<Res
             }
 
             @Override
-            public Object next() {
+            public Map<String, Object> next() {
                 return iterator.next();
             }
 
