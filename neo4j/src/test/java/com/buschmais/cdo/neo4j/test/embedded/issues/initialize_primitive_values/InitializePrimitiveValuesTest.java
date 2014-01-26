b62fc54da0df57d02286db715c2b758b73aaccaa
Lars Martin <lars.martin@smb-tec.com>
2014-01-26 20:32:59 +0100

    #61

---
+++ neo4j/src/test/java/com/buschmais/cdo/neo4j/test/embedded/issues/initialize_primitive_values/InitializePrimitiveValuesTest.java
@@ -0,0 +1,34 @@
+package com.buschmais.cdo.neo4j.test.embedded.issues.initialize_primitive_values;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+import com.buschmais.cdo.api.CdoManager;
+import com.buschmais.cdo.neo4j.test.embedded.AbstractEmbeddedCdoManagerTest;
+import com.buschmais.cdo.neo4j.test.embedded.issues.initialize_primitive_values.composite.A;
+
+public class InitializePrimitiveValuesTest extends
+        AbstractEmbeddedCdoManagerTest {
+
+    @Override
+    protected Class<?>[] getTypes() {
+        return new Class<?>[] { A.class };
+    }
+
+    @Test
+    public void test() {
+        CdoManager cdoManager = getCdoManager();
+        cdoManager.currentTransaction().begin();
+        A a = cdoManager.create(A.class);
+        cdoManager.currentTransaction().commit();
+        cdoManager.currentTransaction().begin();
+        assertThat(a.getB(), nullValue());
+        assertThat(a.isBoolean(), is(false));
+        assertThat(a.getInt(), is(0));
+        cdoManager.currentTransaction().commit();
+    }
+
+}
