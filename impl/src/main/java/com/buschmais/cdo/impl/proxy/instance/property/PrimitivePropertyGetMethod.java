35216990fdcdda5aec740e478da3c38bda994628
Dirk Mahler <dirk.mahler@buschmais.com>
2014-01-27 08:08:59 +0100

    #61 convert null to default values for primitive properties if datastore value is null

--- impl/src/main/java/com/buschmais/cdo/impl/proxy/instance/property/PrimitivePropertyGetMethod.java
+++ impl/src/main/java/com/buschmais/cdo/impl/proxy/instance/property/PrimitivePropertyGetMethod.java
@@ -13,14 +13,37 @@ public class PrimitivePropertyGetMethod<Entity> extends AbstractPropertyMethod<E
 
     public Object invoke(Entity entity, Object instance, Object[] args) {
         PrimitivePropertyMethodMetadata<?> metadata = getMetadata();
+        Object value;
         if (!getPropertyManager().hasProperty(entity, metadata)) {
-            return null;
+            value = null;
+        } else {
+            value = getPropertyManager().getProperty(entity, metadata);
         }
-
-        Object value = getPropertyManager().getProperty(entity, metadata);
         Class<?> type = metadata.getAnnotatedMethod().getType();
+        return convert(value, type);
+    }
+
+    private Object convert(Object value, Class<?> type) {
         if (Enum.class.isAssignableFrom(type)) {
             return Enum.valueOf((Class<Enum>) type, (String) value);
+        } else if (value == null) {
+            if (boolean.class.equals(type)) {
+                return false;
+            } else if (short.class.equals(type)) {
+                return 0;
+            } else if (int.class.equals(type)) {
+                return 0;
+            } else if (long.class.equals(type)) {
+                return 0l;
+            } else if (float.class.equals(type)) {
+                return 0f;
+            } else if (double.class.equals(type)) {
+                return 0d;
+            } else if (char.class.equals(type)) {
+                return 0;
+            } else if (byte.class.equals(type)) {
+                return 0;
+            }
         }
         return value;
     }
