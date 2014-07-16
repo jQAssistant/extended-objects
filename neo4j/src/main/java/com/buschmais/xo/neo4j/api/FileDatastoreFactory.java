dbb2ff8d9480f3b1da8c8a29267954ddaf6cd7c3
Lars Martin <lars.martin@smb-tec.com>
2014-07-16 10:33:04 +0200

    #129
    decode path

--- neo4j/src/main/java/com/buschmais/xo/neo4j/api/FileDatastoreFactory.java
+++ neo4j/src/main/java/com/buschmais/xo/neo4j/api/FileDatastoreFactory.java
@@ -1,19 +1,28 @@
 package com.buschmais.xo.neo4j.api;
 
-import com.buschmais.xo.neo4j.impl.datastore.EmbeddedNeo4jDatastore;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URLDecoder;
+import java.util.Properties;
+
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.factory.GraphDatabaseBuilder;
 import org.neo4j.graphdb.factory.GraphDatabaseFactory;
 
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.util.Properties;
+import com.buschmais.xo.neo4j.impl.datastore.EmbeddedNeo4jDatastore;
 
 public class FileDatastoreFactory implements DatastoreFactory<EmbeddedNeo4jDatastore> {
 
     @Override
     public EmbeddedNeo4jDatastore createGraphDatabaseService(URI uri, Properties properties) throws MalformedURLException {
-        GraphDatabaseBuilder databaseBuilder = new GraphDatabaseFactory().newEmbeddedDatabaseBuilder(uri.toURL().getPath());
+        String path;
+        try {
+            path = URLDecoder.decode(uri.toURL().getPath(), "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            throw new MalformedURLException(e.getMessage());
+        }
+        GraphDatabaseBuilder databaseBuilder = new GraphDatabaseFactory().newEmbeddedDatabaseBuilder(path);
         Properties neo4jProperties = Neo4jPropertyHelper.getNeo4jProperties(properties);
         for (String name : neo4jProperties.stringPropertyNames()) {
             databaseBuilder.setConfig(name, neo4jProperties.getProperty(name));
