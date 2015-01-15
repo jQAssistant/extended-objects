88d82c72a029e505b4e114a6d78dc00bcff2d462
Dirk Mahler <dirk.mahler@buschmais.com>
2015-01-15 10:20:13 +0100

    #161 code cleanup

---
+++ api/src/main/java/com/buschmais/xo/api/XOMigrator.java
@@ -0,0 +1,32 @@
+package com.buschmais.xo.api;
+
+/**
+ * Defines the interface for a type migrator.
+ *
+ * @param <T>
+ *            The type of the instance to migrate.
+ */
+public interface XOMigrator<T> {
+	/**
+	 * Add a type to the instance.
+	 *
+	 * @param newType
+	 *            The new type.
+	 * @param newTypes
+	 *            The new types.
+	 * @return A new instance representing the original types and the given new
+	 *         types.
+	 */
+	CompositeObject add(Class<?> newType, Class<?>... newTypes);
+
+	/**
+	 * Remove a type from the instance.
+	 *
+	 * @param obsoleteType
+	 *            The obsolete type.
+	 * @param obsoleteTypes
+	 *            The obsolete types.
+	 * @return A new instance without the obsolete types.
+	 */
+	CompositeObject remove(Class<?> obsoleteType, Class<?>... obsoleteTypes);
+}
--- impl/src/main/java/com/buschmais/xo/impl/XOMigratorImpl.java
+++ impl/src/main/java/com/buschmais/xo/impl/XOMigratorImpl.java
@@ -1,71 +1,91 @@
 package com.buschmais.xo.impl;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import com.buschmais.xo.api.CompositeObject;
-import com.buschmais.xo.api.XOManager;
+import com.buschmais.xo.api.XOMigrator;
 import com.buschmais.xo.spi.datastore.DatastoreEntityManager;
 import com.buschmais.xo.spi.datastore.DatastoreEntityMetadata;
 import com.buschmais.xo.spi.datastore.DatastoreRelationMetadata;
 import com.buschmais.xo.spi.datastore.TypeMetadataSet;
 import com.buschmais.xo.spi.metadata.type.EntityTypeMetadata;
 
-import java.util.HashSet;
-import java.util.Set;
+/**
+ * Implementation of the {@link com.buschmais.xo.api.XOMigrator} interface.
+ * 
+ * @param <EntityId>
+ *            The type of entity ids as provided by the datastore.
+ * @param <Entity>
+ *            The type of entities as provided by the datastore.
+ * @param <EntityMetadata>
+ *            The type of entity metadata as provided by the datastore.
+ * @param <EntityDiscriminator>
+ *            The type of discriminators as provided by the datastore.
+ */
+public class XOMigratorImpl<T, EntityId, Entity, EntityMetadata extends DatastoreEntityMetadata<EntityDiscriminator>, EntityDiscriminator>
+		implements XOMigrator<T> {
 
-public class XOMigratorImpl<T, EntityId, Entity, EntityMetadata extends DatastoreEntityMetadata<EntityDiscriminator>, EntityDiscriminator> implements
-        XOManager.XOMigrator<T> {
+	private T instance;
 
-    private T instance;
+	private MetadataProvider<EntityMetadata, EntityDiscriminator, ? extends DatastoreRelationMetadata<?>, ?> metadataProvider;
+	private DatastoreEntityManager<EntityId, Entity, EntityMetadata, EntityDiscriminator, ?> datastoreEntityManager;
+	private AbstractInstanceManager<EntityId, Entity> entityInstanceManager;
 
-    private SessionContext<EntityId, Entity, EntityMetadata, EntityDiscriminator, ?, ?, ?, ?, ?> sessionContext;
+	/**
+	 * Constructor.
+	 * 
+	 * @param instance
+	 *            The instance to migrate.
+	 * @param sessionContext
+	 *            The session context.
+	 */
+	XOMigratorImpl(T instance, SessionContext<EntityId, Entity, EntityMetadata, EntityDiscriminator, ?, ?, ?, ?, ?> sessionContext) {
+		this.instance = instance;
+		metadataProvider = sessionContext.getMetadataProvider();
+		this.datastoreEntityManager = sessionContext.getDatastoreSession().getDatastoreEntityManager();
+		this.entityInstanceManager = sessionContext.getEntityInstanceManager();
+	}
 
-    XOMigratorImpl(T instance, SessionContext<EntityId, Entity, EntityMetadata, EntityDiscriminator, ?, ?, ?, ?, ?> sessionContext) {
-        this.instance = instance;
-        this.sessionContext = sessionContext;
-    }
+	@Override
+	public CompositeObject add(Class<?> newType, Class<?>... newTypes) {
+		Set<EntityDiscriminator> newDiscriminators = getDiscriminators(newType, newTypes);
+		Entity entity = invalidateInstance(entityInstanceManager);
+		Set<EntityDiscriminator> entityDiscriminators = datastoreEntityManager.getEntityDiscriminators(entity);
+		newDiscriminators.removeAll(entityDiscriminators);
+		datastoreEntityManager.addDiscriminators(entity, newDiscriminators);
+		return createInstance(entity);
+	}
 
-    @Override
-    public CompositeObject add(Class<?> newType, Class<?>... newTypes) {
-        Set<EntityDiscriminator> newDiscriminators = getDiscriminators(newType, newTypes);
-        AbstractInstanceManager<EntityId, Entity> entityInstanceManager = sessionContext.getEntityInstanceManager();
-        Entity entity = invalidateInstance(entityInstanceManager);
-        DatastoreEntityManager<EntityId, Entity, EntityMetadata, EntityDiscriminator, ?> datastoreEntityManager = sessionContext.getDatastoreSession()
-                .getDatastoreEntityManager();
-        Set<EntityDiscriminator> entityDiscriminators = datastoreEntityManager.getEntityDiscriminators(entity);
-        newDiscriminators.removeAll(entityDiscriminators);
-        datastoreEntityManager.addDiscriminators(entity, newDiscriminators);
-        instance = entityInstanceManager.createInstance(entity);
-        return CompositeObject.class.cast(instance);
-    }
+	@Override
+	public CompositeObject remove(Class<?> obsoleteType, Class<?>... obsoleteTypes) {
+		Set<EntityDiscriminator> obsoleteDiscriminators = getDiscriminators(obsoleteType, obsoleteTypes);
+		Entity entity = invalidateInstance(entityInstanceManager);
+		Set<EntityDiscriminator> entityDiscriminators = datastoreEntityManager.getEntityDiscriminators(entity);
+		obsoleteDiscriminators.retainAll(entityDiscriminators);
+		datastoreEntityManager.removeDiscriminators(entity, obsoleteDiscriminators);
+		return createInstance(entity);
+	}
 
-    @Override
-    public CompositeObject remove(Class<?> obsoleteType, Class<?>... obsoleteTypes) {
-        Set<EntityDiscriminator> obsoleteDiscriminators = getDiscriminators(obsoleteType, obsoleteTypes);
-        AbstractInstanceManager<EntityId, Entity> entityInstanceManager = sessionContext.getEntityInstanceManager();
-        DatastoreEntityManager<EntityId, Entity, EntityMetadata, EntityDiscriminator, ?> datastoreEntityManager = sessionContext.getDatastoreSession()
-                .getDatastoreEntityManager();
-        Entity entity = invalidateInstance(entityInstanceManager);
-        Set<EntityDiscriminator> entityDiscriminators = datastoreEntityManager.getEntityDiscriminators(entity);
-        obsoleteDiscriminators.retainAll(entityDiscriminators);
-        datastoreEntityManager.removeDiscriminators(entity, obsoleteDiscriminators);
-        instance = entityInstanceManager.createInstance(entity);
-        return CompositeObject.class.cast(instance);
-    }
+	private Set<EntityDiscriminator> getDiscriminators(Class<?> type, Class<?>[] types) {
+		TypeMetadataSet<EntityTypeMetadata<EntityMetadata>> typeMetadata = new TypeMetadataSet<>();
+		typeMetadata.add(metadataProvider.getEntityMetadata(type));
+		for (Class<?> currentType : types) {
+			typeMetadata.add(metadataProvider.getEntityMetadata(currentType));
+		}
+		return new HashSet<>(metadataProvider.getEntityDiscriminators(typeMetadata));
+	}
 
-    private Set<EntityDiscriminator> getDiscriminators(Class<?> type, Class<?>[] types) {
-        TypeMetadataSet<EntityTypeMetadata<EntityMetadata>> typeMetadata = new TypeMetadataSet<>();
-        MetadataProvider<EntityMetadata, EntityDiscriminator, ? extends DatastoreRelationMetadata<?>, ?> metadataProvider = sessionContext
-                .getMetadataProvider();
-        typeMetadata.add(metadataProvider.getEntityMetadata(type));
-        for (Class<?> currentType : types) {
-            typeMetadata.add(metadataProvider.getEntityMetadata(currentType));
-        }
-        return new HashSet<>(metadataProvider.getEntityDiscriminators(typeMetadata));
-    }
+	private Entity invalidateInstance(AbstractInstanceManager<EntityId, Entity> entityInstanceManager) {
+		Entity entity = entityInstanceManager.getDatastoreType(instance);
+		entityInstanceManager.removeInstance(instance);
+		entityInstanceManager.closeInstance(instance);
+		return entity;
+	}
 
-    private Entity invalidateInstance(AbstractInstanceManager<EntityId, Entity> entityInstanceManager) {
-        Entity entity = entityInstanceManager.getDatastoreType(instance);
-        entityInstanceManager.removeInstance(instance);
-        entityInstanceManager.closeInstance(instance);
-        return entity;
-    }
+	private CompositeObject createInstance(Entity entity) {
+		instance = entityInstanceManager.createInstance(entity);
+		return CompositeObject.class.cast(instance);
+	}
+
 }
