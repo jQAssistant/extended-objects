43398dff6981983853ca69f6ade3bb656ab040bb
dmahler <dirk.mahler@globalfoundries.com>
2017-03-13 16:05:47 +0100

    update relation ids during flush

--- neo4j/remote/src/main/java/com/buschmais/xo/neo4j/remote/impl/datastore/StatementBatchBuilder.java
+++ neo4j/remote/src/main/java/com/buschmais/xo/neo4j/remote/impl/datastore/StatementBatchBuilder.java
@@ -2,40 +2,82 @@ package com.buschmais.xo.neo4j.remote.impl.datastore;
 
 import static org.neo4j.driver.v1.Values.parameters;
 
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
+import org.neo4j.driver.v1.Record;
 import org.neo4j.driver.v1.Value;
 
 public class StatementBatchBuilder {
 
+    private static class BatchEntry {
+
+        private List<Value> batch = new ArrayList<>();
+        private Callback callback;
+
+        public BatchEntry(Callback callback) {
+            this.callback = callback;
+        }
+
+        void add(Value parameters) {
+            batch.add(parameters);
+        }
+
+        public List<Value> getBatch() {
+            return batch;
+        }
+
+        public Callback getCallback() {
+            return callback;
+        }
+
+    }
+
+    @FunctionalInterface
+    public interface Callback {
+
+        void process(Record result);
+
+    }
+
     private StatementExecutor statementExecutor;
 
-    private Map<String, List<Value>> batches = new HashMap<>();
+    private Map<String, BatchEntry> batches = new HashMap<>();
 
     public StatementBatchBuilder(StatementExecutor statementExecutor) {
         this.statementExecutor = statementExecutor;
     }
 
     public void add(String key, Value parameters) {
-        List<Value> batch = batches.get(key);
+        add(key, parameters, null);
+    }
+
+    public void add(String key, Value parameters, Callback callback) {
+        BatchEntry batch = batches.get(key);
         if (batch == null) {
-            batch = new LinkedList<>();
+            batch = new BatchEntry(callback);
             batches.put(key, batch);
         }
         batch.add(parameters);
     }
 
     public void execute() {
-        for (Map.Entry<String, List<Value>> entry : batches.entrySet()) {
+        for (Map.Entry<String, BatchEntry> entry : batches.entrySet()) {
             String statement = entry.getKey();
-            List<Value> batch = entry.getValue();
-            String batchStatement = "UNWIND {batch} as entry " + statement + " RETURN count(*) as count";
-            statementExecutor.getSingleResult(batchStatement, parameters("batch", batch));
-            // TODO verify result
+            BatchEntry batchEntry = entry.getValue();
+            List<Value> batch = batchEntry.getBatch();
+            if (!batch.isEmpty()) {
+                String batchStatement = "UNWIND {batch} as entry " + statement;
+                Record result = statementExecutor.getSingleResult(batchStatement, parameters("batch", batch));
+                Callback callback = batchEntry.getCallback();
+                if (callback != null) {
+                    callback.process(result);
+                }
+            }
         }
         batches.clear();
     }
+
 }
