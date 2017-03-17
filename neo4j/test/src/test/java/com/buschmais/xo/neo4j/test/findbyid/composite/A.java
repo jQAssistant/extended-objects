f0ad2911cc19fc42b75a1f907a63a231aff5a7f4
Dirk Mahler <dirk.mahler@buschmais.com>
2017-03-17 17:41:57 +0100

    fixed IT

--- neo4j/test/src/test/java/com/buschmais/xo/neo4j/test/findbyid/composite/A.java
+++ neo4j/test/src/test/java/com/buschmais/xo/neo4j/test/findbyid/composite/A.java
@@ -1,8 +1,10 @@
 package com.buschmais.xo.neo4j.test.findbyid.composite;
 
+import com.buschmais.xo.neo4j.api.annotation.Batchable;
 import com.buschmais.xo.neo4j.api.annotation.Label;
 
 @Label
+@Batchable(false)
 public interface A {
 
     A2B getA2B();
--- neo4j/test/src/test/java/com/buschmais/xo/neo4j/test/findbyid/composite/B.java
+++ neo4j/test/src/test/java/com/buschmais/xo/neo4j/test/findbyid/composite/B.java
@@ -1,8 +1,10 @@
 package com.buschmais.xo.neo4j.test.findbyid.composite;
 
+import com.buschmais.xo.neo4j.api.annotation.Batchable;
 import com.buschmais.xo.neo4j.api.annotation.Label;
 
 @Label
+@Batchable(false)
 public interface B {
 
     A2B getA2B();
