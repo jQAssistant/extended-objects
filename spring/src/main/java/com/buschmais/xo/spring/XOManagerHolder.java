b20bff07ebcb0545cc2c9b530a67b97c6e3a7519
Dirk Mahler <dirk.mahler@buschmais.com>
2021-01-25 10:31:55 +0100

    improved transactional XOManager proxy implementation

--- spring/src/main/java/com/buschmais/xo/spring/XOManagerHolder.java
+++ spring/src/main/java/com/buschmais/xo/spring/XOManagerHolder.java
@@ -2,9 +2,11 @@ package com.buschmais.xo.spring;
 
 import com.buschmais.xo.api.XOManager;
 
+import lombok.ToString;
 import org.springframework.transaction.support.ResourceHolderSupport;
 import org.springframework.util.Assert;
 
+@ToString
 public class XOManagerHolder extends ResourceHolderSupport {
 
     private final XOManager xoManager;
