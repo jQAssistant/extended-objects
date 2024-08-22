127cdc36ba0b0de9c97297e7b41f3e00e77cfac6
Dirk Mahler <dirk.mahler@buschmais.com>
2024-08-22 16:37:52 +0200

    log Neo4j result notifications using level debug instead of warning

--- neo4j/spi/src/main/java/com/buschmais/xo/neo4j/spi/CypherQueryResultIterator.java
+++ neo4j/spi/src/main/java/com/buschmais/xo/neo4j/spi/CypherQueryResultIterator.java
@@ -7,20 +7,14 @@ import com.buschmais.xo.api.ResultIterator;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 
-import static java.util.Arrays.stream;
-import static java.util.stream.Collectors.toMap;
-
 @Slf4j
 public abstract class CypherQueryResultIterator implements ResultIterator<Map<String, Object>> {
 
     @Override
     public final void close() {
         for (Notification notification : dispose()) {
-            Notification.Severity severity = notification.getSeverity();
-            if (severity.equals(Notification.Severity.WARNING)) {
-                getLogger().warn("{} - {}: {} (at {}:{})", notification.getCode(), notification.getTitle(), notification.getDescription(),
-                    notification.getLine(), notification.getColumn());
-            }
+            getLogger().debug("{} - {}: {} (at {}:{})", notification.getCode(), notification.getTitle(), notification.getDescription(), notification.getLine(),
+                notification.getColumn());
         }
     }
 
